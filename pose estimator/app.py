f51d29f288034eca7d56a0f97110035603325389
Nayan Kanaparthi <kanaparthinayan@gmail.com>
2024-01-09 19:48:22 +0530

    all errors fixed

--- pose estimator/app.py
+++ pose estimator/app.py
@@ -5,62 +5,85 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-
 pose = mp_pose.Pose(min_tracking_confidence=0.5, min_detection_confidence=0.5)
 
-#video feed
+# video feed
 cap = cv2.VideoCapture(0)
+counter = 0
+stage = None
 
-def calculate_angle(a,b,c):
-    a= np.array(a)
-    b= np.array(b)
-    c= np.array(c)
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
 
-    radians = np.arctan2(c[1]-b[1], c[0],b[0])-np.arctan2(a[1]-b[1],a[0]-b[0])
-    angle = np.abs(180.0*radians/np.pi)
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
 
-    if angle>180.0:
-        angle= 360-angle
+    if angle > 180:
+        angle = 360 - angle
     return angle
 
-
 while cap.isOpened():
-    ret,frame = cap.read()
+    ret, frame = cap.read()
 
+    # color change
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
 
-    # here the frame was captured by open cv and it will represent it in BGR format, but mediapipe analyses the image in RGB so,
-    #color change
-    image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    image.flags.writeable=False
-
-    # feed the changed color image to a     
-
+    # feed the changed color image to a
     results = pose.process(image)
-    image.flags.writeable=True
+    image.flags.writeable = True
 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     try:
         landmarks = results.pose_landmarks.landmark
 
-        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].z]
-        elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].z]
-        wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].z]
+        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                 landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                 landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-        angle = calculate_angle(shoulder,elbow,wrist)
-        
+        # Check if all landmarks are present
+        if all(landmarks):
+            angle = calculate_angle(shoulder, elbow, wrist)
 
+            cv2.putText(image, str(angle),
+                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-    except:
-        pass
+            if angle > 160 and stage != "down":
+                stage = "down"
+            elif angle < 40 and stage == 'down':
+                stage = "up"
+                counter += 1
+                print(counter)
 
+    except Exception as e:
+        print(f"Error: {e}")
 
-    mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS)
+    cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
 
+    # Rep data
+    cv2.putText(image, 'REPS', (15, 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(image, str(counter), (10, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+    # Stage data
+    cv2.putText(image, 'STAGE', (65, 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(image, stage, (60, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-    cv2.imshow('feed',image)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    cv2.imshow('feed', image)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
